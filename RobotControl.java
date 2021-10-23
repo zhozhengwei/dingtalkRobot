dfc05d10f5e5b80b8e6581bf26cb8f63d13685c3
Zhouzhengwei <zzw17370709160@163.com>
2021-10-23 12:54:18 +0800

    Commit

--- RobotControl.java
+++ RobotControl.java
@@ -16,7 +16,7 @@ public class RobotControl {
 	
 	 public static void main(String[] args) throws Exception {
 	        Long timestamp = System.currentTimeMillis();
-	        String secret = "SEC40ed8a11f01fe21ff9d537187542c79f21bbb524af707d9bc09aabcd1db97ac1";
+	        String secret = "SEC***********************";
 
 	        String stringToSign = timestamp + "\n" + secret;
 	        Mac mac = Mac.getInstance("HmacSHA256");
@@ -24,7 +24,7 @@ public class RobotControl {
 	        byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
 	        String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)),"UTF-8");
 	        System.out.println(sign);
-	        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=d834f06998d3f25e828d677350c165a8be6fe65e5e16562a2e62251955f790a4"+"&timestamp="+timestamp+"&sign="+sign);
+	        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=***********************"+"&timestamp="+timestamp+"&sign="+sign);
 	        OapiRobotSendRequest request = new OapiRobotSendRequest();
 	        request.setMsgtype("text");
 	        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
